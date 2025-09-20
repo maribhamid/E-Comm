875686c1600ebe867c08e5f29a844295900e495e
Marib Hamid <maribhamid098@gmail.com>
2025-09-20 15:51:11 +0530

    adding product data as data.js

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import p1 from './assets/products/p1.png';
+import p2 from './assets/products/p2.png';
+import p3 from './assets/products/p3.png';
+import p4 from './assets/products/p4.png';
+import p5 from './assets/products/p5.png';
+import p6 from './assets/products/p6.png';
+
+export const products = [
+  {
+    id: 1,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 4,
+    isHot: true,
+    image: p1,
+    colors: ["blue", "red", "black", "yellow", "pink", "beige"],
+    brand: "Nike"
+  },
+  {
+    id: 2,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 4,
+    isHot: true,
+    image: p2,
+    colors: ["red"],
+    brand: "Adidas"
+  },
+  {
+    id: 3,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 3,
+    isHot: true,
+    image: p3,
+    colors: ["beige"],
+    brand: "Vans"
+  },
+  {
+    id: 4,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 5,
+    isHot: true,
+    image: p4,
+    colors: ["blue", "black"],
+    brand: "Nike"
+  },
+  {
+    id: 5,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 4,
+    isHot: true,
+    image: p5,
+    colors: ["yellow", "blue"],
+    brand: "Nike"
+  },
+  {
+    id: 6,
+    name: "Nike Air Max 270 React",
+    price: 299.43,
+    oldPrice: 534.33,
+    discount: "24% Off",
+    rating: 4,
+    isHot: true,
+    image: p6,
+    colors: ["blue", "yellow"],
+    brand: "Nike"
+  },
+];
